22b33ad95675ab8caa2d9666a3ec2e8e75970665
alvarezpuentesjesus-maker <alvarezpuentesjesus@gmail.com>
2026-09-23 18:31:47 +0200

    Update variables.py

--- Software/dependencias/variables.py
+++ Software/dependencias/variables.py
@@ -11,7 +11,7 @@ Información del archivo:
 - Autor: Jesús Álvarez Puentes
 - Fecha: 2026-07-28
 - Versión: 0.1
-- Licencia: MIT
+- Licencia: GNU General Public License
 - Dependencias:
     * CircuitPython (board, digitalio, analogio, busio, usb_midi)
     * adafruit_midi (MIDI, NoteOn, NoteOff, PitchBend)
