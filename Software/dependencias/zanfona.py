938a1f0b1d8e05419145e083bef1c5b877de97fe
alvarezpuentesjesus-maker <alvarezpuentesjesus@gmail.com>
2026-09-23 18:55:22 +0200

    Update zanfona.py

--- Software/dependencias/zanfona.py
+++ Software/dependencias/zanfona.py
@@ -13,7 +13,7 @@ Información del archivo:
 - Autor: Jesús Álvarez Puentes
 - Fecha: 2026-07-28
 - Versión: 0.1
-- Licencia: GNU General Public License
+- Licencia: CC BY-NC-SA 4.0
 - Dependencias:
     * adafruit_midi (MIDI, NoteOn, NoteOff, PitchBend)
     * usb_midi
