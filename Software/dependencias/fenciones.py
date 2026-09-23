e2bc13bae5ce41a4521d8ecb0e48b8cd58ad2531
alvarezpuentesjesus-maker <alvarezpuentesjesus@gmail.com>
2026-09-23 18:54:56 +0200

    Update fenciones.py

--- Software/dependencias/fenciones.py
+++ Software/dependencias/fenciones.py
@@ -13,7 +13,7 @@ Información del archivo:
 - Autor: Jesús Álvarez Puentes
 - Fecha: 2026-07-28
 - Versión: 0.1
-- Licencia: GNU General Public License
+- Licencia: CC BY-NC-SA 4.0
 - Dependencias:
     * CircuitPython (busio)
     * dependencias.variables
