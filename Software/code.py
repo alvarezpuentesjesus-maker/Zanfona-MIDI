5654b97960de619cb0913c82147b6163bef38461
alvarezpuentesjesus-maker <alvarezpuentesjesus@gmail.com>
2026-09-23 18:54:26 +0200

    Update code.py

--- Software/code.py
+++ Software/code.py
@@ -12,7 +12,7 @@ Información del archivo:
 - Autor: Jesús Álvarez Puentes
 - Fecha: 2026-07-28
 - Versión: 0.1
-- Licencia: GNU General Public License
+- Licencia: CC BY-NC-SA 4.0
 - Dependencias: 
     * CircuitPython (time)
     * dependencias.zanfona
