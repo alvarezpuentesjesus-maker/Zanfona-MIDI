0f35ebfb582150ffc0ae41315aeb85f635244050
alvarezpuentesjesus-maker <alvarezpuentesjesus@gmail.com>
2026-09-23 18:31:14 +0200

    Update MEDIAS.py

--- Software/MEDIAS.py
+++ Software/MEDIAS.py
@@ -11,7 +11,7 @@ Información del archivo:
 - Autor: Jesús Álvarez Puentes
 - Fecha: 2026-07-28
 - Versión: 0.1
-- Licencia: MIT
+- Licencia: GNU General Public License
 - Dependencias:
     * CircuitPython (board, analogio, time)
     * math (Librería estándar)
